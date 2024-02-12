3843c39df336c3b5c592a4a68edb3d791a45d545
Joel Alejandro Espinoza Sánchez <alex.esa18@gmail.com>
2024-02-11 23:17:31 -0600

    Changed landing style. Phase 1 of translation done

--- scripts.js
+++ scripts.js
@@ -6,3 +6,51 @@ function toggle(id) {
         elemento.style.display = "none";
     }
 }
+
+// Laguage management
+
+let language = navigator.language;
+let selectLanguage = document.getElementById('language-select');
+
+// Setting a default language
+selectLanguage.value = 'en';
+document.title = "My Way";
+
+if (language == 'es')
+{
+    selectLanguage.value = 'es';
+    document.title = "Mi Camino";
+}
+if (language == 'en')
+{
+    selectLanguage.value = 'en';
+    document.title = "My Way";
+}
+
+// Changing the text in first load of the page
+fetch('translations/' + selectLanguage.value + '.json')
+    .then(response => response.json())
+    .then(data => {
+        for (var key in data)
+        {
+            document.getElementsByClassName(key)[0].innerHTML = data[key];
+        }
+    })
+    .catch(error => {
+        console.error('Failed while trying to load JSON translation file: ', error);
+    });
+
+// Changing the text depending on the language
+selectLanguage.addEventListener('change', function() {
+    fetch('translations/' + selectLanguage.value + '.json')
+        .then(response => response.json())
+        .then(data => {
+            for (var key in data)
+            {
+                document.getElementsByClassName(key)[0].innerHTML = data[key];
+            }
+        })
+        .catch(error => {
+            console.error('Failed while trying to load JSON translation file: ', error);
+        });
+});
